8c85946816ab3ea0c819de23f77868bfaa8724ed
TOGoS <togos00@gmail.com>
2019-03-10 13:01:29 -0500

    Move SSD1306 classes to their own directory

---
+++ src/arduino/PowerCube/components.cpp
@@ -0,0 +1,6 @@
+// The Arduino compiler doesn't seem to know about cpp
+// files that aren't in the root sketch directory,
+// so we'll include them here.
+
+#include "TOGoS/SSD1306/Controller.cpp"
+#include "TOGoS/SSD1306/Printer.cpp"

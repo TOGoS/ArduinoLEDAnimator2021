38ca198873b9087880e951d384699e065dcea05c
TOGoS <togos00@gmail.com>
2019-05-11 15:56:54 -0500

    Make the FastLEDController a real component

--- src/arduino/PowerCube/PowerCube.ino
+++ src/arduino/PowerCube/PowerCube.ino
@@ -16,29 +16,92 @@
 #include <FastLED.h>
 #include <vector>
 
+uint8_t parseHexDigit(char digit) {
+  if( digit >= '0' && digit <= '9' ) return digit - '0';
+  if( digit >= 'A' && digit <= 'F' ) return 10 + digit - 'A';
+  if( digit >= 'a' && digit <= 'f' ) return 10 + digit - 'a';
+  return 0;
+}
 
-template<template<uint8_t DATA_PIN, EOrder RGB_ORDER> class Controller, uint8_t DATA_PIN, EOrder RGB_ORDER>
-class FastLEDSequencer : TOGoS::PowerCube::Component {
-public:
-  std::vector<CRGB> colorData;
-  FastLEDSequencer(size_t ledCount)
-    : colorData(ledCount)
-  {
-    FastLED.addLeds<Controller, D4, RGB_ORDER>(this->colorData.data(), this->colorData.size());
+uint8_t parseHexPair(const char *digits) {
+  return (parseHexDigit(digits[0]) << 4) | parseHexDigit(digits[1]);
+}
+
+CRGB parseRgb(const TOGoS::StringView &rgbStr) {
+  if( rgbStr.size() == 7 && rgbStr[0] == '#' ) {
+    return CRGB(
+      parseHexPair(rgbStr.begin()+1),
+      parseHexPair(rgbStr.begin()+3),
+      parseHexPair(rgbStr.begin()+5)
+    );
   }
-  void initialize() { }
-  void unshiftColor( CRGB color ) {
-    for( size_t i=colorData.size(); i > 0; --i ) {
-      colorData[i] = colorData[i-1];
+  return CRGB(255,0,255);
+}
+
+TOGoS::StringView hexEncodeRgb(char *buffer, const CRGB &rgb)
+{
+  sprintf(buffer, "#%02X%02X%02X", rgb.red, rgb.green, rgb.blue);
+  return TOGoS::StringView(buffer, 7);
+}
+
+Print &operator<<(Print &printer, const CRGB& rgb) {
+  char buffer[8];
+  hexEncodeRgb(buffer, rgb);
+  return printer << buffer;
+}
+
+namespace TOGoS { namespace PowerCube {
+  template<template<uint8_t DATA_PIN, EOrder RGB_ORDER> class Controller, uint8_t DATA_PIN, EOrder RGB_ORDER>
+  class FastLEDController : public TOGoS::PowerCube::Component {
+  protected:
+    KernelPtr kernel;
+    std::string name;
+  public:
+    std::vector<CRGB> colorData;
+    FastLEDController(KernelPtr kernel, const TOGoS::StringView& name, size_t ledCount)
+      : kernel(kernel), name(name), colorData(ledCount)
+    {
+      this->kernel->getLogStream() << "# Setting up FastLEDController " << this->name << " on data pin " << DATA_PIN << " with " << ledCount << " LEDs\n";
+      FastLED.addLeds<Controller, DATA_PIN, RGB_ORDER>(this->colorData.data(), this->colorData.size());
     }
-    colorData[0] = color;
-  }
-  void update() {
-    //this->controller.showLeds();   // Don't do this it makes you have seizures.
-    FastLED.show();
-  }
-};
-FastLEDSequencer<WS2812B, D4, GRB> fastLedController(11);
+    void unshiftColor( CRGB color ) {
+      for( size_t i=colorData.size(); i > 0; --i ) {
+        colorData[i] = colorData[i-1];
+      }
+      colorData[0] = color;
+    }
+    virtual void update() override {
+      //this->controller.showLeds();   // Don't do this it makes you have seizures.
+      //this->kernel->getLogStream() << "# FastLED.show()\n";
+      FastLED.show(); // This actually only needs to be done once for all controllers
+    }
+    virtual void onMessage(const ComponentMessage& m) override {
+      if( m.path.length >= 2 && m.path[m.path.length-2] == "pixelcolors" && m.path[m.path.length-1] == "unshift" ) {
+        CRGB color = parseRgb(m.payload);
+        this->kernel->getLogStream() << "# " << this->name << "Unshifting color " << color << " lol\n";
+        this->unshiftColor(color);
+      }
+      if( m.path.length >= 1 && m.path[m.path.length-1] == "debugloop" ) {
+        this->debugLoop();
+      }
+    }
+
+    uint8_t debugPhase = 0;
+    void debugLoop() {
+      this->kernel->getLogStream() << "# " << this->name << " entering debug loop\n";
+      while( true ) {
+        ++debugPhase;
+        if( debugPhase >= 8 ) debugPhase = 0;
+        this->unshiftColor( CRGB(debugPhase << 5, debugPhase << 4, debugPhase << 3));
+        FastLED.show();
+        delay(500);
+      }
+    }
+  };
+}}
+
+
+
 
 // TODO (story): read temperature and humidity, show on OLED screen
 // TODO (story): turn on and off a relay based on messages from serial port
@@ -55,15 +118,15 @@ TOGoS::SSD1306::Printer oledPrinter(oledController, font8x8);
 TOGoS::PowerCube::Kernel kernel;
 
 void dumpPinList() {
-  Serial.print("# D1 = "); Serial.println(D1);
-  Serial.print("# D2 = "); Serial.println(D2);
-  Serial.print("# D3 = "); Serial.println(D3);
-  Serial.print("# D4 = "); Serial.println(D4);
-  Serial.print("# D5 = "); Serial.println(D5);
-  Serial.print("# D6 = "); Serial.println(D6);
-  Serial.print("# D7 = "); Serial.println(D7);
-  Serial.print("# D8 = "); Serial.println(D8);
-  Serial.print("# BUILTIN_LED = "); Serial.println(BUILTIN_LED);
+  Serial << "# D1 = " << D1 << " (used by 2wire)\n";
+  Serial << "# D2 = " << D2 << "\n";
+  Serial << "# D3 = " << D3 << "\n";
+  Serial << "# D4 = " << D4 << "\n";
+  Serial << "# D5 = " << D5 << "\n";
+  Serial << "# D6 = " << D6 << "\n";
+  Serial << "# D7 = " << D7 << "\n";
+  Serial << "# D8 = " << D8 << "\n";
+  Serial << "# BUILTIN_LED = " << BUILTIN_LED << "\n";
 }
 
 void setup() {
@@ -71,6 +134,9 @@ void setup() {
 
   delay(500);
 
+  // Wire uses D1, which I want to use for controlling a WS2812B strip.
+  // Therefore commenting out the OLED screen stuff.
+  /*
   Wire.begin();
   oledController.initialize();
   oledController.displayOn();
@@ -84,6 +150,7 @@ void setup() {
     oledPrinter.println(i);
     delay(1000);
   }
+  */
 
   dumpPinList();
 
@@ -93,8 +160,7 @@ void setup() {
   kernel.components["dht7"].reset(new TOGoS::PowerCube::DHTReader(&kernel, "dht7", D7, DHT22));
   kernel.components["builtinled"].reset(new TOGoS::PowerCube::DigitalSwitch(&kernel, "builtinled", BUILTIN_LED, true));
   kernel.components["d5"].reset(new TOGoS::PowerCube::DigitalSwitch(&kernel, "d5", D5, false));
-
-  fastLedController.initialize();
+  kernel.components["fastleds"].reset(new TOGoS::PowerCube::FastLEDController<WS2812B, D1, GRB>(&kernel, "fastleds", 11));
 }
 
 int brightnessDirection = 1;
@@ -122,6 +188,7 @@ TriangleOscillator bOsc(0, 23, 0, 255);
 void loop() {
   kernel.update();
 
+  /*
   if( kernel.getCurrentTickNumber() % 100 == 0 ) {
     if( brightness == 0 && brightnessDirection < 0 ) {
       brightnessDirection = 1;
@@ -135,14 +202,22 @@ void loop() {
     oledPrinter.print("Brightness: ");
     oledPrinter.println(brightness);
   }
+  */
 
   unsigned long currentTime = millis();
   if( lastColorPush == 0 || currentTime - lastColorPush > 50 ) {
-    fastLedController.unshiftColor( CRGB(rOsc.next(), gOsc.next(), bOsc.next()) );
+    using ComponentMessage = TOGoS::PowerCube::ComponentMessage;
+    using Path = TOGoS::PowerCube::Path;
+    char formattedColor[8];
+    TOGoS::StringView rgbStr = hexEncodeRgb(formattedColor, CRGB(rOsc.next(), gOsc.next(), bOsc.next()));
+    kernel.deliverMessage(ComponentMessage(
+      Path() << "fastleds" << "pixelcolors" << "unshift",
+      rgbStr,
+      TOGoS::PowerCube::PubBits::Internal
+    ));
+    Serial << "# Want to unshift pixel color: " << rgbStr << "\n";
     lastColorPush = currentTime;
   }
-  
-  fastLedController.update();
 }
 
 int hiCount = 0;

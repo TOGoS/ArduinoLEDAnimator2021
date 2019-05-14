4198b00817366aafd3f710e55f4795ec99399157
TOGoS <togos00@gmail.com>
2019-05-14 15:08:57 -0500

    PWMSwitch uses analogWrite by default instead of trying to do PWM itself

--- src/arduino/PowerCube/TOGoS/PowerCube/PWMSwitch.cpp
+++ src/arduino/PowerCube/TOGoS/PowerCube/PWMSwitch.cpp
@@ -6,9 +6,17 @@ namespace TOGoS { namespace PowerCube {
     pinMode(this->pin, OUTPUT);
   }
   void PWMSwitch::set(float level) {
+    if( level < 0 ) level = 0;
+    if( level > 1 ) level = 1;
+    if( this->isActiveLow ) level = 1 - level;
+#if TOGOS_POWERCUBE_PWMSWITCH_MODE==TOGOS_POWERCUBE_PWMSWITCH_MODE_EMULATED
     this->level = this->targetLevel = level * 65536;
+#else
+    analogWrite(this->pin, level * PWMRANGE);
+#endif
   }
   void PWMSwitch::update() {
+#if TOGOS_POWERCUBE_PWMSWITCH_MODE==TOGOS_POWERCUBE_PWMSWITCH_MODE_EMULATED
     bool targetSubstate = this->level > 32768;
     if( targetSubstate != this->currentInstantaneousState ) {
       digitalWrite(this->pin, (targetSubstate != this->isActiveLow) ? HIGH : LOW);
@@ -17,6 +25,7 @@ namespace TOGoS { namespace PowerCube {
     int instLevel = this->currentInstantaneousState ? 65536 : 0;
     int quantError = this->level - instLevel;
     this->level = this->targetLevel + quantError;
+#endif
   }
   void PWMSwitch::onMessage(const ComponentMessage& cm) {
     if( cm.path.length == 3 && cm.path[0] == this->name && (cm.path[1] == "state" || cm.path[1] == "level") && cm.path[2] == "set" ) {
--- src/arduino/PowerCube/TOGoS/PowerCube/PWMSwitch.h
+++ src/arduino/PowerCube/TOGoS/PowerCube/PWMSwitch.h
@@ -5,6 +5,12 @@
 
 #include "Kernel.h"
 
+#define TOGOS_POWERCUBE_PWMSWITCH_MODE_ANALOGWRITE 1 // Use the platform's built-in PWM capabilities
+#define TOGOS_POWERCUBE_PWMSWITCH_MODE_EMULATED 2
+#ifndef TOGOS_POWERCUBE_PWMSWITCH_MODE
+#define TOGOS_POWERCUBE_PWMSWITCH_MODE TOGOS_POWERCUBE_PWMSWITCH_MODE_ANALOGWRITE
+#endif
+
 namespace TOGoS { namespace PowerCube {
   /** Reads commands from a stream (e.g. Serial) nd forwards them */
   class PWMSwitch : public Component {
@@ -13,9 +19,11 @@ namespace TOGoS { namespace PowerCube {
     std::string name;
     uint8_t pin;
     bool isActiveLow;
+#if TOGOS_POWERCUBE_PWMSWITCH_MODE==TOGOS_POWERCUBE_PWMSWITCH_MODE_EMULATED
     int32_t targetLevel;
     int32_t level;
     bool currentInstantaneousState = 0;
+#endif    
   public:
     PWMSwitch(KernelPtr kernel, const StringView &name, uint8_t pin, bool isActiveLow);
     void set(float level);

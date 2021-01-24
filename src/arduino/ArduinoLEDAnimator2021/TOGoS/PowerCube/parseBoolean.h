5b8b6cd52a1a0b3ba3d30785573ca0ab208f2b99
TOGoS <togos00@gmail.com>
2021-01-24 02:51:59 -0600

    Delete unused PowerCube code

--- src/arduino/ArduinoLEDAnimator2021/TOGoS/PowerCube/parseBoolean.h
+++
@@ -1,5 +0,0 @@
-#include "../StringView.h"
-
-namespace TOGoS { namespace PowerCube {
-  bool parseBoolean(const StringView &text, bool &result);
-}}
--- src/arduino/ArduinoLEDAnimator2021/TOGoS/PowerCube/parseEnableMessage.h
+++
@@ -1,5 +0,0 @@
-#include "Kernel.h"
-
-namespace TOGoS { namespace PowerCube {
-  bool parseEnableMessage(const ComponentMessage &cm, bool &enabled);
-}}
